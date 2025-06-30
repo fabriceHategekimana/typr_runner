df9d5d40e4765a18a65fda24a6acd595bdd7d274
Fabrice <fab.hatege15@gmail.com>
2025-06-30 03:22:31 +0200

    update

--- R/typr_runner.R
+++ R/typr_runner.R
@@ -21,5 +21,6 @@ run_typr_file <- function() {
   
   # Envoyer la commande au terminal
   #rstudioapi::terminalExecute(commande1, workingDir = repertoire, env = env_vars, show = TRUE)
-  rstudioapi::terminalExecute(commande1, workingDir = repertoire, show = TRUE)
+  #rstudioapi::terminalExecute(commande1, workingDir = repertoire, show = TRUE)
+  rstudioapi::terminalExecute(commande1, show = TRUE)
 }
