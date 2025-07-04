49d7bcc7995265a96ec2d4758ef52d8dbf6f2449
Fabrice <fab.hatege15@gmail.com>
2025-07-05 01:16:36 +0200

    update

--- R/typr_runner.R
+++ R/typr_runner.R
@@ -1,3 +1,11 @@
+get_executable <- function() {
+	if (.Platform$OS.type == "windows") {
+		"typr.exe"
+	} else {
+		"typr" 
+	}
+}
+
 #' Run TypR files in terminal
 #' @export
 run_typr_file <- function() {
@@ -28,10 +36,9 @@ run_typr_project <- function() {
   nom_fichier <- basename(context$path)
   repertoire <- dirname(context$path)
   
-	exe_path <- system.file("bin", "typr", package = "typr.runner")
-
+	exe_path <- system.file("bin", get_executable(), package = "typr.runner")
 	result <- system(paste0(exe_path, " run"), intern = TRUE)
-	print(paste(result, collapse = "\n"))
+	print(paste(result, collapse = "\r"))
 }
 
 #' Build TypR project in terminal
@@ -45,6 +52,10 @@ build_typr_project <- function() {
   
   commande1 <- paste0("/home/fabrice/sh/typr build", " 2>&1")
   rstudioapi::terminalExecute(commande1, workingDir = repertoire, show = TRUE)
+
+	exe_path <- system.file("bin", get_executable(), package = "typr.runner")
+	result <- system(paste0(exe_path, " build"), intern = TRUE)
+	print(paste(result, collapse = "\r"))
 }
 
 #' Check TypR project in terminal
